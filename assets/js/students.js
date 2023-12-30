9c3605751f31a49377ce9c8d6e27edc6e3d2ed76
KingZingBoss <114019096+KingZingBoss@users.noreply.github.com>
2023-12-30 11:44:03 +0100

    a

--- assets/js/students.js
+++ assets/js/students.js
@@ -24,3 +24,30 @@ function animateValue(id, start, end, duration) {
     requestAnimationFrame(fn);
 }
 animateValue('students', 0, 50, 3000);
+
+function animateValue(id, start, end, duration) {
+    var range = end - start;
+    var current = start;
+    var obj = document.getElementById(id);
+    var starttime;
+    var fn = (ms) => {
+        let progress = 0;
+        if(starttime === undefined) {
+            starttime = ms;
+        } else {
+            progress = ms - starttime;
+            if (progress >= duration) {
+                // the `+ ' ' + progress + 'ms';` is just to show the duration, wouldn't use that in final code
+                current = end.toLocaleString() + "+"  ;
+            } else {
+                current = start + Math.floor(progress/duration * range);
+            }
+        }
+        obj.innerHTML = current.toLocaleString();
+        if (progress < duration) {
+            requestAnimationFrame(fn);
+        }
+    };
+    requestAnimationFrame(fn);
+}
+animateValue('students2', 0, 50, 3000);
